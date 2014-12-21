7b6e939b6c8e8640cda1892385088fbe592960fb
BANK.CPE <BANK.CPE@BANK-CPE>
2014-12-21 09:07:52 +0700

    first commit

---
+++ src/main/java/com/company/chapter2/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.company.chapter2;
+
+/**
+ *
+ * @author BANK.CPE
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
