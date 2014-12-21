6767ff60b2fa723955c0a2c941a8c4d7278535f5
BANK.CPE <BANK.CPE@BANK-CPE>
2014-12-21 11:58:33 +0700

    first show even commit

--- src/main/java/com/company/chapter2/ShowEven.java
+++ src/main/java/com/company/chapter2/ShowEven.java
@@ -15,10 +15,10 @@ public class ShowEven {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
-        
+        System.out.println("size : "+size);
         for(int i=1;i<=size;i++){
             if(i%2 == 0){
-                System.out.println(" " +i);
+                System.out.println("number : " +i);
             }
         }
     }
